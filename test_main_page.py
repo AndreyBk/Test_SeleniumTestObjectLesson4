e695a3a71172e2147e2dbb646b77042741b7a7e8
AChernousov <andrey.chernousov@digitalplant.ru>
2019-12-30 16:09:48 +0300

    перехват исключения

--- test_main_page.py
+++ test_main_page.py
@@ -3,10 +3,10 @@ import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import sys
 from .pages.main_page import MainPage
 
-
-#
+# pytest -v --tb=line --language=en test_main_page.py
 # @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"])
 
 
@@ -16,9 +16,15 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()
 
-    # browser.get(link)
-    # login_link = browser.find_element_by_css_selector("#login_link")
-    # login_link.click()
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+# browser.get(link)
+# login_link = browser.find_element_by_css_selector("#login_link")
+# login_link.click()
 
 # def test_espaniolLocation(browser,link):
 #     browser.get(link)
